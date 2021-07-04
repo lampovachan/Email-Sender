3a788c8290c708334eba87d97d8dda82dcead70f
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-07-04 14:57:59 +0300

    add jackson mapping

--- producer/src/main/java/com/tkachuk/producer/kafka/config/KafkaProducerConfig.java
+++ producer/src/main/java/com/tkachuk/producer/kafka/config/KafkaProducerConfig.java
@@ -1,15 +1,18 @@
 package com.tkachuk.producer.kafka.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
-import com.tkachuk.dto.Data;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.messaging.Message;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,23 +20,30 @@ import java.util.Map;
 /**
  * This class contains Kafka configuration.
  */
+
 @Configuration
 public class KafkaProducerConfig {
-    @Value("${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
+    private final KafkaProducerProperties producerProperties;
 
-    @Bean
-    public ProducerFactory<String, Data> producerFactory() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(config);
+    public KafkaProducerConfig(KafkaProducerProperties producerProperties) {
+        this.producerProperties = producerProperties;
     }
 
     @Bean
-    public KafkaTemplate<String, Data> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    KafkaTemplate<String, Message<?>> kafkaTemplate() {
+        return new KafkaTemplate<>(kafkaProducerConfiguration());
+    }
+
+    private ObjectMapper objectMapper() {
+        return Jackson2ObjectMapperBuilder.json()
+                .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .build();
+    }
+
+    private ProducerFactory<String, Message<?>> kafkaProducerConfiguration() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, producerProperties.getBootstrapServers());
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        return new DefaultKafkaProducerFactory<>(properties, new StringSerializer(), new JsonSerializer<>(objectMapper()));
     }
 }
